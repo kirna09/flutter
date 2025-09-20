7e30d8e426348cea407791033efcf53250da672f
andrik <assshh351@gmail.com>
2025-09-20 00:02:23 -0700

    practica 3 completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mi_app/src/pages/app.dart';
+import 'package:mi_app/src/pages/home_page.dart'; // Add this import if HomePage is defined here
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Practica 01', home: HomePage());
+  }
 }
