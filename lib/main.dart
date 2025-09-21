2cc6f1b5cae0b4ef44053110b0069b12dc1ed641
andrik <assshh351@gmail.com>
2025-09-20 18:51:01 -0700

    practica 4 completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:mi_app/src/pages/app.dart';
+// import 'package:mi_app/src/pages/app.dart';
+import 'package:mi_app/src/pages/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'practica 03',
+      home: SplashScreen(),
+    );
+  }
 }
