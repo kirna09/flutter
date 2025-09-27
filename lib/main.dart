3d9faf2efe18bbe459b5624e35e99f8f04ffd213
andrik <assshh351@gmail.com>
2025-09-27 16:15:11 -0700

    practica 6 completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:mi_app/src/pages/app.dart';
+import 'package:mi_app/src/pages/tabs_page.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Practica 06',
+      home: TabsPage(),
+    );
+  }
 }
