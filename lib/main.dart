f6a26281a3e20029624b7cedb60398ecd5fe8862
andrik <assshh351@gmail.com>
2025-10-12 19:07:19 -0700

    practica 14

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:mi_app/src/pages/app.dart';
+import 'package:mi_app/src/pages/crud_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "practica 14",
+      home: const CrudPage(),
+    );
+  }
 }
